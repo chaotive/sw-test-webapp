959407c466b73b258657a96625fc4769abf5d7e7
Leonardo Montenegro <leonardomontenegro123@gmail.com>
2019-08-03 19:21:48 -0400

    Prototyping StarshipsView using Bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-const App: React.FC = () => <div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
+// const App: React.FC = () => <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.tsx</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
+
+class StarshipsView extends React.Component {
+  public render() {
+    return <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  }
+}
+
+const App: React.FC = () => <StarshipsView />
 
 export default App
