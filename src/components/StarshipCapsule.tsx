5eb0b215260ba58488e4cc0d04b4e7e951e21647
chaotive <leonardomontenegro123@gmail.com>
2019-08-05 16:04:00 -0400

    Extracting starship capsule parameters

--- src/components/StarshipCapsule.tsx
+++ src/components/StarshipCapsule.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import {Starship} from '../../typings/swapi/starships'
 import Card from 'react-bootstrap/Card'
 
-const StarshipCapsule: React.FC<Starship> = props => <Card className="m-4">
-  <Card.Body><strong>{props.name}</strong></Card.Body>
-  <Card.Body>{props.crew}</Card.Body>
-  <Card.Body>{props.passengers}</Card.Body>
-  <Card.Body>{props.hyperdrive_rating}</Card.Body>
-</Card>
+const StarshipCapsule: React.FC<Starship> = props => {
+  const {name, crew, passengers, hyperdrive_rating} = props
+  return <Card className="m-4">
+    <Card.Body><strong>Name: {name}</strong></Card.Body>
+    <Card.Body>Crew: {crew}</Card.Body>
+    <Card.Body>Passengers: {passengers}</Card.Body>
+    <Card.Body>Hyperdrive Class: {hyperdrive_rating}</Card.Body>
+  </Card>
+}
 
 export default StarshipCapsule
