89e509f0bbfb4a1c6349ede303d0f70ff58187e1
Leonardo Montenegro <leonardomontenegro123@gmail.com>
2019-08-05 00:43:47 -0400

    Adding loading spinner.

--- src/components/fragments.tsx
+++ src/components/fragments.tsx
@@ -1,8 +1,13 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import React from 'react'
+import Spinner from 'react-bootstrap/Spinner'
 
-export const loadingFragment = <Row><Col>Loading...</Col></Row>
+export const loadingFragment = <Row><Col className="text-center m-4">
+  <Spinner animation="border" role="status">
+    <span className="sr-only">Loading...</span>
+  </Spinner>
+</Col></Row>
 
 export const titleFragment = (text: string) => <Row>
   <Col as="h1">{text}</Col>
