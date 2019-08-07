4d0c193dad2e9063ee5f49042a65ce4ddb9fc7ee
Leonardo Montenegro <leonardomontenegro123@gmail.com>
2019-08-07 00:31:43 -0400

    Adding more test cases for starship capsule

--- src/components/StarshipCapsule.test.tsx
+++ src/components/StarshipCapsule.test.tsx
@@ -3,23 +3,39 @@ import ReactDOM from 'react-dom'
 import StarshipCapsule, {IStarshipCapsule} from './StarshipCapsule'
 import renderer from 'react-test-renderer'
 
-const testData: IStarshipCapsule = {
-    name: 'testShip',
-    crew: '5',
-    passengers: 'some',
-    hyperdrive_rating: '0.1'
-}
-
 it('renders without crashing', () => {
+  const testData: IStarshipCapsule = {
+    name: 'testShip',
+    crew: '0',
+    passengers: '10',
+    hyperdrive_rating: '3.4'
+  }
 
-    const div = document.createElement('div')
-    ReactDOM.render(<StarshipCapsule {...testData} />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement('div')
+  ReactDOM.render(<StarshipCapsule {...testData} />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
 
-it('renders correctly', () => {
-    const tree = renderer
-      .create(<StarshipCapsule {...testData} />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+it('renders correctly, hyperdrive_rating as number', () => {
+  const testData: IStarshipCapsule = {
+    name: '123-532-M',
+    crew: '5',
+    passengers: 'unknown',
+    hyperdrive_rating: '1'
+  }
+
+  const tree = renderer.create(<StarshipCapsule {...testData} />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders correctly, hyperdrive_rating as unknown', () => {
+  const testData: IStarshipCapsule = {
+    name: 'testShip',
+    crew: '5',
+    passengers: '4900',
+    hyperdrive_rating: 'unknown'
+  }
+
+  const tree = renderer.create(<StarshipCapsule {...testData} />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
